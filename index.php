3ff0b3f1891b8a48092df62fdb41dba629e2353d
Anthelme-RISSER <anthelme.risser@gmail.com>
2016-04-13 15:52:49 +0200

    reglage page 404
    animation 404 fonctionnelle

--- index.php
+++ index.php
@@ -17,7 +17,16 @@ else if ($q == 'movie')
 else 
 	$page = '404';
 
+
+if($q == '' || $q == 'search' || $q == 'movie'){
 include 'controllers/'.$page.'.php';
 include 'views/partials/header.php';
 include 'views/pages/'.$page.'.php';
 include 'views/partials/footer.php';
+}
+
+else{
+    include 'controllers/'.$page.'.php';
+    include 'views/pages/'.$page.'.php';
+    include 'views/partials/footer.php';
+}
--- views/pages/404.php
+++ views/pages/404.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -12,8 +12,3 @@
         <div class='error-404'></div>
     </div>
 </section>
- -->
-
- <section>
-     404
- </section>

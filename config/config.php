9795e489ec41324fc43c23f3133c0f3e4f81e4e8
clemishow <clement.saulnier@hetic.net>
2016-04-14 14:55:17 +0200

    config hetic.saulnier.fr

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 	// SALT
 	define('SALT', '69z2EiQ0yglzQ');
 
-	// DB CONNEXION
+	// DB CONNEXION LOCAL
 	define('DB_HOST','localhost');
 	define('DB_NAME','intensive-week');
 	define('DB_USER','root');
@@ -20,3 +20,20 @@
 	{
 	    die('Cound not connect');
 	}
+
+	// // DB CONNEXION HETIC.SAULNIER.FR
+	// define('URL','http://hetic.saulnier.fr/');
+	// define('DB_HOST','mysql51-64.pro');
+	// define('DB_NAME','saulnierfam');
+	// define('DB_USER','saulnierfam');
+	// define('DB_PASS','matador78'); 
+
+	// try
+	// {
+	//     $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+	//     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+	// }
+	// catch (Exception $e)
+	// {
+	//     die('Cound not connect');
+	// }

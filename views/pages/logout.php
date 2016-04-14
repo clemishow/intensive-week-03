aa63e16b4f1ad6684741d91194dd239620ad0354
clemishow <clement.saulnier@hetic.net>
2016-04-14 14:47:39 +0200

    destroy sessions

--- views/pages/logout.php
+++ views/pages/logout.php
@@ -1,7 +1,10 @@
 <?php 
-
-	// Suppression des variables de session et de la session
+	// DESTROY SESSION
 	$_SESSION = array();
 	session_destroy();
+	// COOKIE RESET
+	setcookie('first-name', '', time() + 365*24*3600, null, null, false, true);
+    setcookie('last-name', '', time() + 365*24*3600, null, null, false, true);
+    //REDIRECTION
 	header('location:' . URL . 'account');
 ?>

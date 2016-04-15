368146d47181dbf09352ef0b2ea3b91f21658281
Anthelme-RISSER <anthelme.risser@gmail.com>
2016-04-15 14:35:05 +0200

    player frame repair

--- includes/player-frame.php
+++ includes/player-frame.php
@@ -3,6 +3,6 @@ id="player"
 width="470" 
 height="280" 
 type="text/html" 
-src="https://www.youtube.com/embed/<?= $video->url ?>?vq=tiny&enablejsapi=1&vq=tiny&rel=0&showinfo=0" 
+src="https://www.youtube.com/embed/<?= $video[0]->url ?>?vq=tiny&enablejsapi=1&vq=tiny&rel=0&showinfo=0" 
 frameborder="0">
 </iframe>
